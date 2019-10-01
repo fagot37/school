68ef983e2ae51fd6d1646b6de414e0bf61859697
def <fagot37@mail.ru>
2019-10-01 15:18:23 +0300

    init yii framework minimal

--- web/index.php
+++ web/index.php
@@ -1,2 +1,7 @@
 <?php
-echo '<h1>Video School</h1>';
+    define('YII_DEBUG', true);
+    require __DIR__.'/../vendor/autoload.php';
+    require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
+    $config = require __DIR__.'/../config/web.php';
+    (new yii\web\Application($config))->run();
+?>
